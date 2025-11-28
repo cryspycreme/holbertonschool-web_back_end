edb631d3c897cdd374f783a9dc415e0def877cb9
cryspycreme <crystalchiamm@gmail.com>
2025-11-28 14:08:34 +1100

    Add task 3

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module for async functions"""
+
+import time
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    task = asyncio.create_task(wait_random(max_delay))
+    return task

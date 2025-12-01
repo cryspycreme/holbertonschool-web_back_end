b9a03e2a053734d9a64796b1f2fafb087c98011a
cryspycreme <crystalchiamm@gmail.com>
2025-12-01 17:31:11 +1100

    Document function

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,5 +10,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """Return a list of floats using async_comprehension"""
     result = [i async for i in async_generator()]
     return result

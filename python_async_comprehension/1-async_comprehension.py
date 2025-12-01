05956e0834ebb2e28394f08555a32271f73ee866
cryspycreme <crystalchiamm@gmail.com>
2025-12-01 17:29:47 +1100

    Add 1-async_comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module for async comprehension functions"""
+
+
+import asyncio
+import random
+from typing import Generator, List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    result = [i async for i in async_generator()]
+    return result

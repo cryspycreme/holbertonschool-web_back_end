e7326514beb4f9b068aa35cd3b0c99ac73debf97
cryspycreme <crystalchiamm@gmail.com>
2025-12-13 12:27:09 +1100

    Update task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,7 +4,8 @@
 def schools_by_topic(mongo_collection, topic):
 	"""Return schools with topic"""
 	query_filter = {'topics': { '$regex': topic}}
-	mongo_collection.find(query_filter)
+	schools = list(mongo_collection.find(query_filter))
+	return schools
 
 if __name__ == '__main__':
 	schools_by_topic()

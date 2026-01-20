bbc2dad6e12edcc760c1c23b50a2f07b0b812e8d
“cryspycreme” <11529@holbertonstudents.com>
2026-01-20 06:59:57 +0000

    Update DataView

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,13 @@
 export default function createInt8TypedArray(length, position, value) {
-  // create Int8Array of the given lengrth
-  const typedArray = new Int8Array(length);
-
   // check if position is valid
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
-  // set the value at the given position
-  typedArray[position] = value;
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
 
-  // return the underlying ArrayBuffer
-  return typedArray.buffer;
+  view.setInt8(position, value);
+
+  return view;
 }

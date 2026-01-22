76e03506eb8a4fa542a6ffac22460647e95004a0
“cryspycreme” <11529@holbertonstudents.com>
2026-01-22 05:26:04 +0000

    Fix ex 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,21 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promiseA = signUpUser();
-  const promiseB = uploadPhoto();
+  const promiseA = signUpUser(firstName, lastName);
+  const promiseB = uploadPhoto(fileName);
 
-  const promises = [promiseA, promiseB];
+  return Promise.allSettled([promiseA, promiseB]).then((results) =>
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return result;
+      }
 
-  return Promise.allSettled(promises).then(([user, photoResponse]) => {
-    [
-      {
-        status: 'fulfilled',
-        value: user,
-      },
-      {
-        status: 'fulfilled',
-        value: photoResponse,
-      },
-    ];
-  });
+      return {
+        status: 'rejected',
+        value: result.reason.toString(),
+      };
+    })
+  );
 }

e48eceee4ec10f41cb3de420f5814dd5e8ea5358
“cryspycreme” <11529@holbertonstudents.com>
2026-01-22 03:34:12 +0000

    Return promise object

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,8 +2,13 @@ export default function handleResponseFromAPI(promise) {
   promise
     .then((result) => {
       console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
     })
     .catch((errorMessage) => {
       console.log('Error thrown');
+      return new Error();
     });
 }

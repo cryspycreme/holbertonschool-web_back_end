909cffa3bf55055966398a6f9d13cf3939fd514b
“cryspycreme” <11529@holbertonstudents.com>
2026-01-22 03:31:02 +0000

    Remove promise creation given promise already resolved

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  const myPromise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error());
-    }
-  });
-  // ..then what happens after a resolve?
-  myPromise
+  promise
     .then((result) => {
       console.log('Got a response from the API');
     })
